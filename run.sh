9f72537222e6172d3e80282a71b856c81c5d8ac2
Jeff Ching <jching@avvo.com>
2015-05-13 14:31:35 -0700

    skip the HOME environment variable

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-for i in `env`
+for i in `env | grep -v HOME`
 do
   echo "export $i" >> /home/jenkins/.bashrc
 done
-env > /home/jenkins/environment.properties
+env | grep -v HOME > /home/jenkins/environment.properties
 /usr/sbin/sshd -D
