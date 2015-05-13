6fa4f07492c909930716d9c56c2a94c51d3499de
Jeff Ching <jching@avvo.com>
2015-05-13 16:07:23 -0700

    install non-interactive

--- install_phantomjs.sh
+++ install_phantomjs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get install build-essential g++ flex bison gperf ruby perl \
+apt-get install -y build-essential g++ flex bison gperf ruby perl \
   libsqlite3-dev libfontconfig1-dev libicu-dev libfreetype6 libssl-dev \
   libpng-dev libjpeg-dev python
 
