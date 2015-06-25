ed3842f0f8de422c3a92818555111cebb22b2a70
Kyle Leaders <kyle.leaders@gmail.com>
2015-06-25 10:34:08 -0700

    Now can dynamically provision

--- jenkins_worker.sh
+++ jenkins_worker.sh
@@ -1,17 +1,44 @@
 #!/bin/bash
 
-COUNT=4
+COUNT=${2-4}
+RAMDISK=${RAMDISK-/mnt/ramdisk}
+TEMPLATE=${TEMPLATE-$PWD/template.yml}
+
+test -d $RAMDISK || ("RAMDISK: '$RAMDISK' doesn't exist" && exit 1)
+test -f $TEMPLATE || ("TEMPLATE: '$TEMPLATE' doesn't exist" && exit 1)
+
+function create_ramdisk() {
+  if test -d ${RAMDISK}/mysql${1}
+  then
+    echo "Ramdisk for mysql ${1} exists, skipping"
+  else
+    echo "Creating ramdisk dir ${1}"
+    mkdir ${RAMDISK}/mysql${1}
+  fi
+}
+
+function create_yaml() {
+  cp ${1} docker-compose${2}.yml
+  sed -i "s/\$NUM/$2/g" docker-compose${2}.yml
+}
+
+function docker-service {
+  echo docker-compose -p jenkins$1 -f docker-compose$1.yml $2
+  docker-compose -p jenkins$1 -f docker-compose$1.yml $2
+}
 
 function start() {
-  for i in `seq 1 $COUNT`
+  for i in `seq 1 $1`
   do
+    create_ramdisk $i
+    create_yaml $TEMPLATE $i
     echo starting $i
     docker-service $i "up -d"
   done
 }
 
 function stop() {
-  for i in `seq 1 $COUNT`
+  for i in `seq 1 $1`
   do
     echo stopping $i
     docker-service $i stop
@@ -20,7 +47,7 @@ function stop() {
 }
 
 function restart() {
-  for i in `seq 1 $COUNT`
+  for i in `seq 1 $1`
   do
     echo restarting $i
     docker-service $i stop
@@ -29,20 +56,15 @@ function restart() {
   done
 }
 
-function docker-service {
-  echo docker-compose -p jenkins$1 -f docker-compose$1.yml $2
-  docker-compose -p jenkins$1 -f docker-compose$1.yml $2
-}
-
 case $1 in
 start)
-  start
+  start $COUNT
   ;;
 stop)
-  stop
+  stop $COUNT
   ;;
 restart)
-  restart
+  restart $COUNT
   ;;
 *)
   echo "unknown command '$1'"
