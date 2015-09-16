2e930f5641917200fcd5f90795fa34fadfd6c272
Kyle Leaders <kyle.leaders@gmail.com>
2015-09-16 10:40:42 -0700

    Reducing log noise

--- install_phantomjs.sh
+++ install_phantomjs.sh
@@ -13,7 +13,7 @@
 # install 1.9.8 from https://gist.github.com/julionc/7476620
 apt-get install -y build-essential chrpath libssl-dev libxft-dev libfreetype6 libfreetype6-dev libfontconfig1 libfontconfig1-dev
 export PHANTOM_JS="phantomjs-1.9.8-linux-x86_64"
-wget https://bitbucket.org/ariya/phantomjs/downloads/$PHANTOM_JS.tar.bz2
+wget -q https://bitbucket.org/ariya/phantomjs/downloads/$PHANTOM_JS.tar.bz2
 sudo tar xvjf $PHANTOM_JS.tar.bz2
 mv $PHANTOM_JS /usr/local/share
 ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/bin
