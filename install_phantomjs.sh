fc32155311acaa87db879952c59739514bc5e33c
Jeff Ching <ching.jeff@gmail.com>
2016-07-06 14:44:41 -0700

    need to install wget for phantomjs

--- install_phantomjs.sh
+++ install_phantomjs.sh
@@ -2,7 +2,9 @@
 export PHANTOM_JS="phantomjs-1.9.8-linux-x86_64"
 export DEST="/usr/local/share"
 
-apt-get install -y build-essential chrpath libssl-dev libxft-dev libfreetype6 libfreetype6-dev libfontconfig1 libfontconfig1-dev
+set -e
+
+apt-get install -y build-essential chrpath libssl-dev libxft-dev libfreetype6 libfreetype6-dev libfontconfig1 libfontconfig1-dev wget
 mkdir -p $DEST
 wget -O - -q https://bitbucket.org/ariya/phantomjs/downloads/$PHANTOM_JS.tar.bz2 | tar -xvjC $DEST
 ln -s ${DEST}/${PHANTOM_JS}/bin/phantomjs /usr/local/bin/
