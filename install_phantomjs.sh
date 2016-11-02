1a17e8b6d7e68c0c33953ea454102182396fcab4
Seth Ringling <sringling@avvo.com>
2016-11-02 07:46:55 -0700

    Install the latest phantom js 2.1.1 in addition to 1.9.8

--- install_phantomjs.sh
+++ install_phantomjs.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-export PHANTOM_JS="phantomjs-1.9.8-linux-x86_64"
-export DEST="/usr/local/share"
 
-set -e
+phantom_versions=(phantomjs-1.9.8-linux-x86_64 phantomjs-2.1.1-linux-x86_64)
 
-apt-get install -y build-essential chrpath libssl-dev libxft-dev libfreetype6 libfreetype6-dev libfontconfig1 libfontconfig1-dev wget
-mkdir -p $DEST
-wget -O - -q https://bitbucket.org/ariya/phantomjs/downloads/$PHANTOM_JS.tar.bz2 | tar -xvjC $DEST
-ln -s ${DEST}/${PHANTOM_JS}/bin/phantomjs /usr/local/bin/
+for phantom_version in ${phantom_versions[@]}; do
+    export PHANTOM_JS=${phantom_version}
+    export DEST="/usr/local/share"
+
+    set -e
+
+    apt-get install -y build-essential chrpath libssl-dev libxft-dev libfreetype6 libfreetype6-dev libfontconfig1 libfontconfig1-dev wget
+    mkdir -p $DEST
+    wget -O - -q https://bitbucket.org/ariya/phantomjs/downloads/$PHANTOM_JS.tar.bz2 | tar -xvjC $DEST
+    ln -s ${DEST}/${PHANTOM_JS}/bin/phantomjs /usr/local/bin/
+
+done
